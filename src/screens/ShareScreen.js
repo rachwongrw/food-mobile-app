69970a2204bf1e0c8fec93d17d4ab4d0e7fc1b05
Rachel Wong <rwrachelwong@gmail.com>
2020-05-30 16:05:34 -0700

    creating a food app (from Udemy course)

---
+++ src/screens/ShareScreen.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+
+const SearchScreen = () => {
+  return <View>
+    <Text> Share screen </Text>
+  </View>
+}
+
+export default SearchScreen
